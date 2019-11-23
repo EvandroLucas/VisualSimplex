06f739e84dd7cbea4bc5127bbfff5407389c6245
Matheus Vargas <m6.vargas@gmail.com>
2019-11-23 19:57:24 -0300

    WIP Implementação B&B

---
+++ src/main/java/app/BBMain.java
@@ -0,0 +1,48 @@
+package app;
+
+import branchAndBound.BBSolver;
+import logging.Logger;
+import lpp.CanonicalLPP;
+import lpp.LPP;
+import lpp.LPPReader;
+import print.CustomPrinter;
+import simplex.Tableau;
+
+import java.io.File;
+import java.util.Objects;
+
+public class BBMain {
+
+    public static void main(String[] args) {
+        try {
+            // Find the file
+            String inputFileName;
+            File inputFile;
+            inputFileName = "lppInput/simpleInput.txt";
+            Logger.println("info", "Loading file : " + inputFileName);
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            inputFile = new File(Objects.requireNonNull(classLoader.getResource(inputFileName)).getFile());
+            // Read and parse the file content
+            LPPReader lppReader = new LPPReader();
+            LPP lpp = lppReader.readFromFile(inputFile);
+            System.out.println(lpp);
+            // Convert the Linear Programming Problem to a  Canonical Linear Programming Problem
+            CanonicalLPP lpp2 = new CanonicalLPP(lpp);
+            System.out.println(lpp2);
+            // Generate a tableau
+            Tableau tableau = lpp2.getTableau();
+            CustomPrinter printer = new CustomPrinter();
+            printer.printTableau(tableau);
+            // Solve, just to see...
+            LPPSolver lppSolver = new LPPSolver();
+
+            Logger.println("Info","Solving Integer LPP");
+            BBSolver bbSolver = new BBSolver();
+            bbSolver.solve(lpp2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/branchAndBound/BBSolver.java
+++ src/main/java/branchAndBound/BBSolver.java
@@ -1,8 +1,10 @@
 package branchAndBound;
+
 import app.LPPSolver;
 import lpp.CanonicalLPP;
 import lpp.Solver;
 import simplex.result.Result;
+
 import java.util.Queue;
 import java.util.concurrent.PriorityBlockingQueue;
 
@@ -10,20 +12,25 @@ public class BBSolver implements Solver {
 
     private Queue<BBNode> queue = new PriorityBlockingQueue<>();
 
-    public Result solve(CanonicalLPP originalLPP){
+    public Result solve(CanonicalLPP originalLPP) {
 
-        Result resultFirstNode = new LPPSolver().solve(originalLPP);
+        LPPSolver lppSolver = new LPPSolver();
+
+        CanonicalLPP originalLPPCopy = originalLPP;
+
+        Result resultFirstNode = lppSolver.solve(originalLPP);
         BBNode firstNode = new BBNode(originalLPP, resultFirstNode);
 
         queue.add(firstNode);
-        LPPSolver lppSolver = new LPPSolver();
-        while( ! queue.isEmpty()) {
-            BBNode node = queue.remove();
-            Result result = lppSolver.solve(node.lpp);
-            if(){
-                
-            }
-        }
+//        LPPSolver lppSolver = new LPPSolver();
+//        while( ! queue.isEmpty()) {
+//            BBNode node = queue.remove();
+//            Result result = lppSolver.solve(node.lpp);
+//            if(){
+//
+//            }
+//        }
+        return null;
     }
 
 }
