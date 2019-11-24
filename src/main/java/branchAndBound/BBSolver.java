3df4fe1c7fefdeb2c07e99a20dd88ced9083144a
Matheus Vargas <m6.vargas@gmail.com>
2019-11-23 23:21:18 -0300

    WIP Implementação B&B

--- src/main/java/branchAndBound/BBSolver.java
+++ src/main/java/branchAndBound/BBSolver.java
@@ -2,10 +2,13 @@ package branchAndBound;
 
 import app.LPPSolver;
 import lpp.CanonicalLPP;
+import lpp.Restriction;
+import lpp.RestrictionType;
 import lpp.Solver;
 import numbers.Value;
 import simplex.result.Result;
 
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.concurrent.PriorityBlockingQueue;
 
@@ -17,14 +20,27 @@ public class BBSolver implements Solver {
 
         LPPSolver lppSolver = new LPPSolver();
 
-        CanonicalLPP originalLPPCopy = originalLPP;
-
         Result resultFirstNode = lppSolver.solve(originalLPP);
 
-        for(Value value : resultFirstNode.getSolution()) {
+        for(int solutionIndex = 0; solutionIndex < resultFirstNode.getSolution().length; solutionIndex++) {
+
+            Value value = resultFirstNode.getSolution()[solutionIndex];
+
             if(!value.isInteger()) {
                 Value newValueGraterEquals = value.roundUp();
+                Value [] leftSide =  new Value[resultFirstNode.getSolution().length];
+                Arrays.fill(leftSide, 0);
+                Value v = new Value();
+                v.assign(1);
+                leftSide[solutionIndex] = v;
+                CanonicalLPP lppCopy = originalLPP;
+                RestrictionType rtt = RestrictionType.GreaterOrEqualThan;
+                lppCopy.restrictions.add(new Restriction(leftSide, newValueGraterEquals, rtt, "x"));
+
+
                 Value newValueLessEquals = newValueGraterEquals.sub(1.0);
+                CanonicalLPP lppCopy2 = originalLPP;
+
             }
         }
 
